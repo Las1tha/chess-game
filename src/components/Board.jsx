844be8ecaee7212695e87b674241bbe050f135b9
Las1tha <lasithakelum55@gmail.com>
2025-09-18 15:38:53 +0530

    update

---
+++ src/components/Board.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Square from "./Square";
+import '../styles/Board.css';   
+
+const Board =()=>{
+    
+}
+
+export default Board;
---
+++ src/components/Piece.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import '../styles/Piece.css';
+
+const Piece = ({piece})=>{
+   const pieceSymbols = {
+     white:{
+         king: '♔',
+      queen: '♕',
+      rook: '♖',
+      bishop: '♗',
+      knight: '♘',
+      pawn: '♙'
+     },
+     black:{
+        king: '♚',
+        queen: '♛',
+        rook: '♜',
+        bishop: '♝',
+        knight: '♞',
+        pawn: '♟'
+     }
+   };
+   const symbol = pieceSymbols[piece.color][piece.type];
+
+   return(
+    <div className={`piece ${piece.color}`}>
+        {symbol}
+    </div>
+   )
+}
+
+export default Piece;
